fb0b1c3bee291323a09d64bf8772bad30ce4f3aa
DieFirrex <154069304+DieFirrex@users.noreply.github.com>
2024-08-04 17:06:49 +0300

    Add files via upload

---
+++ launch.py
@@ -0,0 +1,29 @@
+import pygame
+import main
+import math
+pygame.init()
+
+'''----------start----------'''
+# Завантаження фонового зображення
+background_image = pygame.image.load('background.png')  # Замість 'background.jfif' вкажіть шлях до вашого зображення фону
+background_image = pygame.transform.scale(background_image, (800, 500)) # задання розмірів фонового зображення
+'''-----------end-----------'''
+
+window = pygame.display.set_mode((800, 500))
+clock = pygame.time.Clock()
+player = main.Player((400,250))
+game = True
+white = (255, 255, 255)
+black = (0, 0, 0)
+while game:
+    window.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    player.events(event)
+    window.blit(background_image, (0, 0))
+    window.blit(player.image, player.rect)
+
+    clock.tick(30)
+    pygame.display.update()
